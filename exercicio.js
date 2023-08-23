78fe598489343954043220beda54cc499ecc6ee5
skjunior <142546585+skjunior@users.noreply.github.com>
2023-08-22 21:45:54 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,30 @@
+/*
+* Sergio krug
+*/
+
+
+//Resposta 1
+
+let A = 11;
+let b = 4;
+console.log(b);
+b = 5;
+console.log(A, b);
+
+//Resposta 2
+
+//nesse caso o conteudo das "caixas" são trocados.
+// exemplo:
+// let = 10 
+//let = 20
+// C passa a ter 10
+//B passa a ser vazio
+// A passa a ter 20
+
+//Resposta 3
+
+let horaTrabalho;
+let ganhoDia;
+
+horaTrabalho = prompt("Quantas horas você trabalha por dia?");
+ganhoDia = prompt("Quanto você recebe por dia?");
