b397eb54ca8406382f01ed8686ed08e4c91d652a
skjunior <142546585+skjunior@users.noreply.github.com>
2023-08-22 21:47:19 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+let variavelTexto = "texto";
+let variavelNumero = 6;
+console.log(typeof variavelTexto);
+console.log(typeof variavelNumero);
+
+
+//variael vazia
+let variavelVazia;
+console.log(typeof variavelVazia);
+let variavelNula = null;
+console.log(variavelNula);
+
+//variavel nula
+let a ="b"
+console.log(a);
+console.log(typeof a);
+
+a = null
+console.log(a);
+console.log(typeof a);
+
+//exercicio2
+
+let nome;
+let idade;
+let numero = 6;
+
+nome = prompt("Qual é o seu nome?");
+idade = prompt("Qual é sua idade?");
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(numero + 5);
+console.log(numero.toString() + 5);
+console.log(idade.toString());
+
+// conversão numero
+
+let n = "42";
+console.log(Number(n));
+console.log(typeof n);
+console.log(typeof Number(n));
